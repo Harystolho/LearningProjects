8bbbf9716ff8e61fa621162573a3b5e794db8b91
Harystolho <haristolhogamer@gmail.com>
2019-07-21 21:37:22 -0300

    Add CommandHandler

--- tda/src/main/java/com/harystolho/tda/server/command/CommandDispatcher.java
+++ tda/src/main/java/com/harystolho/tda/server/command/CommandDispatcher.java
@@ -1,11 +1,30 @@
 package com.harystolho.tda.server.command;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import com.harystolho.tda.shared.QueryResult;
 
 public class CommandDispatcher {
 
-	public QueryResult dispatch(Command<?> command) {
+	private Map<Class<? extends Command<?>>, CommandHandler<? extends Command<?>>> handlers;
+
+	public CommandDispatcher() {
+		handlers = new ConcurrentHashMap<>();
+	}
+
+	public <C extends Command<?>> void register(Class<C> clazz, CommandHandler<C> handler) {
+		handlers.put(clazz, handler);
+	}
+
+	@SuppressWarnings("unchecked")
+	public <C extends Command<?>> QueryResult dispatch(C command) {
+		CommandHandler<C> handler = (CommandHandler<C>) handlers.get(command.getClass());
+
+		if (handler != null)
+			return handler.handle(command);
+
 		return null;
 	}
-	
+
 }
