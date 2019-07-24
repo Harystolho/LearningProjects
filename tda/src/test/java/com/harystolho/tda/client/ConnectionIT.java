f97ec01b12f12a8e7ce3e3b26ca895dfd393ef0a
Harystolho <haristolhogamer@gmail.com>
2019-07-24 20:48:47 -0300

    Add Failsafe plugin to run integration tests

---
+++ tda/src/test/java/com/harystolho/tda/client/ConnectionIT.java
@@ -0,0 +1,14 @@
+package com.harystolho.tda.client;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ConnectionIT {
+
+	@Test
+	public void temp() {
+		
+	}
+	
+}
