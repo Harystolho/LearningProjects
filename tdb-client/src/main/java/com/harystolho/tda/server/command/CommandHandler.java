24b241f56f250a4b52ebc2221cc92f7b440538bf
Harystolho <haristolhogamer@gmail.com>
2019-07-25 19:46:53 -0300

    Move tdb-server to new project

--- tdb-client/src/main/java/com/harystolho/tda/server/command/CommandHandler.java
+++
@@ -1,9 +0,0 @@
-package com.harystolho.tda.server.command;
-
-import com.harystolho.tda.shared.QueryResult;
-
-public interface CommandHandler<C extends Command<?>> {
-
-	public QueryResult handle(C command);
-	
-}
--- tdb-client/src/main/java/com/harystolho/tda/server/transaction/CommandLogger.java
+++
@@ -1,7 +0,0 @@
-package com.harystolho.tda.server.transaction;
-
-public interface CommandLogger {
-
-	public void log(LogBlock logBlock);
-	
-}
